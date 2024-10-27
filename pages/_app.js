c7db974f9d4877e7db29f6d44a93d57a6a85996a
bufflepro <bufflepro@gmail.com>
2024-10-27 16:32:51 +0530

    UI Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import "../global.css";
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+
+
+    return <div style={{ margin:"0 auto"}}><Component {...pageProps} /></div>
   }
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,10 +1,44 @@
 const themeConfig = {
-    logo: <span>My Nextra Documentation</span>,
-    project: {
-      link: 'https://github.com/shuding/nextra',
-    },
-    // ... other theme options
-  };
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Buffle Pro Docs" />
+      <meta property="og:description" content="Your go-to productivity tool." />
+      <link rel="icon" href="/images/favicon.svg" sizes="any" />
+      <title>Buffle Docs</title>
+    </>
+  ),
+  logo: <img src="/images/logo.svg" style={{ width: "180px" }} />,
+
+  banner: {
+    key: "1",
+    content: null,
+  },
+
+  color: {
+    hue: 35,
+  },
+  backgroundColor:{
+    dark:'0,0,0'
+  },
+
+  sidebar:{
+    toggleButton: false,
   
-  export default themeConfig;
-  
+  },
+
+  footer: {
+    component: null,
+  },
+  gitTimestamp: false,
+  themeSwitch: false,
+  feedback: {
+    content: "Submit feeback",
+    useLink: () => "mailto:support@buffle.pro",
+  },
+  editLink: {
+    component: null,
+  },
+};
+
+export default themeConfig;
