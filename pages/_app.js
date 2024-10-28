0c352cfa6399c787664d2345d08d3651fee656ff
bufflepro <bufflepro@gmail.com>
2024-10-28 12:31:25 +0530

    Fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import { useRouter } from 'next/router';
 import "../global.scss";
 
+  
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    // Forcefully apply the `dark` class to <html> or <body>
+    document.documentElement.classList.add("dark");
+  }, []);
+
   const router = useRouter();
   const isEmbedded = router.query.embed === 'true';
 
